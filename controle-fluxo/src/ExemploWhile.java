053c5cb40ca6b42a9abaf8c3fdd0fd6d9028d59e
Ulisses Moraes <ulissesmoraes74@gmail.com>
2024-07-21 18:25:41 -0400

    JAVA - While

---
+++ controle-fluxo/src/ExemploWhile.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.ThreadLocalRandom;;
+
+public class ExemploWhile {
+
+    public static void main(String[] args) {
+
+        double mesada = 50.0;
+
+        while (mesada > 0) {
+
+            Double valorDoce = 8.0;
+
+            if (valorDoce <= mesada) {
+
+                System.out.println("Valor Doce r$ " + valorDoce + " Saldo Mesada r$ " + (mesada = mesada - valorDoce));
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("Fim do Programa!");
+        System.out.println("Meu saldo é de r$" + mesada);
+    }
+
+}
---
+++ controle-fluxo/src/TheadLocalRandom.java
@@ -0,0 +1,9 @@
+
+public class TheadLocalRandom {
+
+    public static Object current() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'current'");
+    }
+
+}
